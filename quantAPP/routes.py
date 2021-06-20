995b38d46020c8e1e09b4ea0bad9c8821453787e
Weslei Silva Bomfim <www.weslei@gmail.com>
2021-06-20 10:18:17 -0300

    frontend

--- quantAPP/routes.py
+++ quantAPP/routes.py
@@ -29,12 +29,16 @@ def home():
     )
 
 
-asset = pd.read_sql_query(
-    "SELECT  empresa, cod, Valor, qtde, total FROM asset_portfolio", db.get_engine())
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
-@asset_portfolio_bp.route('/asset', methods=['GET'])
+
+@asset_portfolio_bp.route('/asset_portfolio', methods=['GET'])
 def asset_portfolio():
+    asset = pd.read_sql_query(
+        "SELECT  empresa, cod, Valor, qtde, total FROM asset_portfolio", db.get_engine())
     return render_template(
         'asset_portfolio.html',
         asset=asset,
